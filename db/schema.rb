72d87b87cf7ec2dbf8af3a60ac5777889f8fee70
Mnelson1992 <nelson.matthew2015@gmail.com>
2018-02-15 09:33:11 -0500

    Added recipe modela dn migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180215141243) do
+ActiveRecord::Schema.define(version: 20180215143011) do
 
   create_table "ingredients", force: :cascade do |t|
     t.string "name"
     t.integer "quantity"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "recipes", force: :cascade do |t|
+    t.string "name"
+    t.string "instructions"
+    t.string "cooktime"
+    t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
