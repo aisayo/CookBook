f440699c1e26d4a3fe1a58ac87d6f398e42a77ba
Mnelson1992 <nelson.matthew2015@gmail.com>
2018-02-16 14:47:09 -0500

    Removed quantity from ingredients table

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180215144050) do
+ActiveRecord::Schema.define(version: 20180216194231) do
 
   create_table "ingredients", force: :cascade do |t|
     t.string "name"
-    t.integer "quantity"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
