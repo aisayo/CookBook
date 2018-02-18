764d98fc73f2dd0bee546d0c12f4b238166be802
Mnelson1992 <nelson.matthew2015@gmail.com>
2018-02-18 17:00:30 -0500

    Set up omniauth with facebook

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
 
-  devise_for :users
+  #devise_for :users
   root 'welcome#home'
 
   resources :recipes
   resources :ingredients
+
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
