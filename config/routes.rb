f82c14cd28071ac60fed1ac738b7fecb0c81ed86
Mnelson1992 <nelson.matthew2015@gmail.com>
2018-02-19 09:40:19 -0500

    Built basic nested resources in the routes folder

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,11 @@ Rails.application.routes.draw do
   #devise_for :users
   root 'welcome#home'
 
-  resources :recipes
-  resources :ingredients
+  resources :recipes do
+    resources :ingredients, only: [:show, :index, :destroy]
+  end
+
+  resources :ingredients, only: [:show, :index, :destroy]
 
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
