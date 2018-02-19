7ef5b3ece3edbc2ed60fae66185889c471a489e0
Mnelson1992 <nelson.matthew2015@gmail.com>
2018-02-18 19:51:58 -0500

    Cleaned up database and styled the ingredients show page

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -15,10 +15,16 @@ class IngredientsController < ApplicationController
 
   def index
     @ingredients = Ingredient.all
-  end 
+  end
 
   def show
     @ingredient = Ingredient.find(params[:id])
+  end
+
+  def destroy
+    @ingredient = Ingredient.find(params[:id])
+    @ingredient.delete
+    redirect_to ingredients_path
   end
 
 
