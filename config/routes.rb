15fd8fa58981e19cd05f36106cf0ad3a76aa4c0d
Mnelson1992 <nelson.matthew2015@gmail.com>
2018-02-20 06:43:49 -0500

    Removed the nested resource to place on the user instead

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,9 @@ Rails.application.routes.draw do
   #devise_for :users
   root 'welcome#home'
 
-  resources :recipes do
-    resources :ingredients, only: [:show, :index, :destroy, :new]
-  end
-
-  resources :ingredients, only: [:show, :index, :destroy]
+  resources :recipes
+  resources :ingredients
+  
 
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
